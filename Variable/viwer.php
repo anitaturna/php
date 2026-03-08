6ea44695a4df8a320770597ea835782b8804c701
anitaturna <anitaturna345@gmail.com>
2026-03-08 12:05:19 +0600

    new codes

---
+++ Variable/viwer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+   echo $_REQUEST ['usname'];
+
+?>
+    <form action="#" method="post">
+        user name: <br>
+        <input type="text" name="usname"> <br> <br>
+        E-mail: <br>
+        <input type="text" name="mail" id=""> <br> <br>
+        <input type="submit" value="submit" name="" id=""> 
+
+    </form>
+</body>
+</html>
