a2419a00c69e5266239c070e2f5db7ac3f32e612
anitaturna <anitaturna345@gmail.com>
2026-03-09 12:02:04 +0600

    new files

---
+++ super-global-variable/get_post_.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3> 
+     super global variable
+     pre define variables:  $_REQUEST,$_GET,$_POST
+    </h3>
+
+
+    
+    <div> 
+        <form action="" method="post"> 
+            Name: 
+            <input type="text" name="n"  id=""> <br>  <br>
+            <input type="submit" name="" value="Submit"  id="">
+        </form>
+    </div>
+
+
+    <?php
+    $store = $_POST['n'];
+    // echo "Name:".$store;
+    print "Name:".$store;
+
+    ?>
+    
+</body>
+</html>
---
+++ super-global-variable/isset_function.php
@@ -0,0 +1,12 @@
+<?php
+//isset variavle and value 2 tai check dey
+
+$name ="Turna";
+if(isset($name)){
+    echo "Found";
+
+} else {
+    echo "variable & value not found ";
+}
+
+?>
