39cc008940f8e4dbd48e6cc550d676c477758198
anitaturna <anitaturna345@gmail.com>
2026-03-29 12:04:57 +0600

    error

--- Oop/access modifier.php
+++ Oop/access modifier.php
@@ -4,15 +4,36 @@ class Student{
     private $age = "22";
     protected $degree ="BSC";
 
+    private function pInfo(){
+        echo "This is private method";
+            echo "<br>";
+    }
+
+    public function pInfoDetails(){
+        echo "This is private full method".$this->pInfo();
+    
+    }
+
+
     public function fullinfo(){
         echo $this->name; 
         echo "<br>";
         echo $this->age;
         echo "<br>";
         echo $this->degree;
+        echo "<br>";
     }
 
 }
+
+
+class Child extends Student{
+    public function show(){
+        echo "my degree".$this->degree;
+    }
+}
+
+
  $result = new Student();
  echo $result -> name;
  echo "<br>";
@@ -21,7 +42,8 @@ class Student{
 //  echo $result -> degree;
 //  echo "<br>";
  $result->fullinfo();
-
+ $child =new Child();
+ $child->pInfoDetails();
 
 
 ?>
