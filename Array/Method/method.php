c9b0e93dcb07de2d79f388a12313f5bddd1909f9
anitaturna <anitaturna345@gmail.com>
2026-04-02 10:19:22 +0600

    list method

---
+++ Array/Method/method.php
@@ -0,0 +1,16 @@
+<?php
+$a = [
+    [1, "Mira", 30, "e@gmail.com", "+5676979"],
+    [2, "Mira", 30, "e@gmail.com", "+5676979"],
+    [3, "Mira", 30, "e@gmail.com", "+5676979"],
+    [4, "Mira", 30, "e@gmail.com", "+5676979"],
+];
+
+//for variable assign
+foreach($a as list($id, $name, $age, $gmail, $phn)){
+    echo "$id | $name | $phn <br>";
+}
+
+
+
+?>
