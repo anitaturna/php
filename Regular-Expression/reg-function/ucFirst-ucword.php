b9b68ed730683aabd7d70341777786ac0c539caa
anitaturna <anitaturna345@gmail.com>
2026-04-06 10:51:11 +0600

    upper-lower

---
+++ Regular-Expression/reg-function/ucFirst-ucword.php
@@ -0,0 +1,16 @@
+<?php
+$a = "there is a new river padma";
+ echo ucfirst($a);
+ echo "<br>";
+
+echo ucwords($a);
+echo "<br>";
+ 
+
+echo strtoupper($a);
+echo "<br>";
+
+echo strtolower($a);
+echo "<br>";
+
+ ?>
