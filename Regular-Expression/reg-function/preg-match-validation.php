a6153b3fd1cf7eaa90b77f0cc9b1e5f26746916d
anitaturna <anitaturna345@gmail.com>
2026-04-07 12:04:41 +0600

    mail validation

---
+++ Regular-Expression/reg-function/preg-match-validation.php
@@ -0,0 +1,12 @@
+<?php
+
+//mail validation
+$email = "Bi-joy@gmail.com";
+$p = "/^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+
+echo preg_match($p, $email);
+?>
+
+<!--  [a-zA-Z0-9._+-] eta mainly @ এর আগে (username part)  -->
+  <!-- @[a-zA-Z0-9.-] mane holo @ + domain name -->
+   <!-- \.[a-zA-Z]{2,}$  mane holo এটা .com / .bd / .org part -->
