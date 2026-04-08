d38abe593a94d385d73e9cbd19d9a0b2298566df
anitaturna <anitaturna345@gmail.com>
2026-04-08 12:26:24 +0600

    image-upload-vlidation

--- file-upload-evidace/img-upload/img-file-validation.php
+++ file-upload-evidace/img-upload/img-file-validation.php
@@ -1,31 +1,38 @@
+<?php
+if(isset($_POST['submitbtn'])){
+    $fileName = $_FILES['f']['name'];
+    $tmp = $_FILES['f']['tmp_name'];
+    $typ = pathinfo($fileName, PATHINFO_EXTENSION);
+    $s = "img/";
+
+    if($typ == "jpg" || $typ == "png"){
+        move_uploaded_file($tmp, $s . $fileName);
+    } else {
+        echo "only jpg, png uploaded";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Upload</title>
 </head>
-
-    <?php
-    if(isset($_POST['submitbtn'])){
-        $fileName = $_FILES['f']['name']; //name mane holo file name
-        $tmp = $_FILES['f']['tmp_name']; //tmp mane loaction
-        $typ = pathinfo($fileName, PATHINFO_EXTENSION); //file type ber korar jonno 
-        $s = "img/";
-        if($typ == "jpg" || $typ == "png"){ //condition for file type
-        move_uploaded_file("$tmp", "$s.$fileName");
-         }
-         else {
-            echo "only jpg, png uploaded";
-         } //move_uploaded_file("form", "to");
-    }
-
-?>
 <body>
-    <form action="" method="post" enctype="multipart/form-data"> 
-        <input type="file" name="f" id="">
-        <input type="submit" name="submitbtn" id="">
 
-    </form>
+<form action="" method="post" enctype="multipart/form-data"> 
+    <input type="file" name="f">
+    <input type="submit" name="submitbtn">
+</form>
+
+<?php
+if(isset($_POST['submitbtn'])){
+    echo "<img src='img/$fileName' width='200'>";
+    echo "<br>";
+    echo "File uploded & Stored successfully";
+}
+?>
+
 </body>
 </html>
